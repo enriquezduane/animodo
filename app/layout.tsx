383546f77a8cbd065163762b59cb033b98e964a9
Duane Enriquez <duanequez@gmail.com>
2025-07-26 23:44:55 +0800

    change to poppins font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import './globals.css'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
 
 export const metadata = {
   title: 'Animodo Dashboard',
@@ -12,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 } 
